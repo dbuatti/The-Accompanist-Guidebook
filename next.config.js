00241415b21ce27f7008e3b6f2a4b90028eef515
[dyad] <daniele.buatti@gmail.com>
2026-05-11 13:07:59 +1000

    [dyad] Upgraded Zod to 3.24.1 and added it to transpilePackages to fix the module export error. - wrote 2 file(s)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@neondatabase/auth", "@neondatabase/auth-ui"],
+  transpilePackages: ["@neondatabase/auth", "@neondatabase/auth-ui", "zod"],
 };
 
 export default nextConfig;
