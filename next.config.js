3673e2ea637ac1aae28b997d7c19e47159164b41
[dyad] <daniele.buatti@gmail.com>
2026-05-11 13:04:19 +1000

    [dyad] Updated next.config.js to transpile Neon Auth packages and pinned Zod version to fix the module resolution error. - wrote 2 file(s) + extra files edited outside of Dyad

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@neondatabase/auth", "@neondatabase/auth-ui"],
 };
 
 export default nextConfig;
